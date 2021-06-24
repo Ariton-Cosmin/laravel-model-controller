84edd20d65b0e9d52144a0ead9fa6e3039a855ac
Cosmin-Ariton <cosminiris12@gmail.com>
2021-06-24 15:53:22 +0200

    HomeController.php-second

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
-    }
 
+        $movies = Movie::all();
+        dump($movies);
+        $data = [
+            'movies' => $movies
+        ];
+
+        return view('home', $data);
+    }
 }
